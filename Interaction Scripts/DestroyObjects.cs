21fa2b080aa627fca8b09e2930b73eb45b4bb529
Wynn502 <119078416+Wynn502@users.noreply.github.com>
2023-04-17 15:42:49 +0100

    Update DestroyObjects.cs

--- Interaction Scripts/DestroyObjects.cs
+++ Interaction Scripts/DestroyObjects.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class DestroyObjects : MonoBehaviour
 {
-    public string targetTag; // ָ���� Tag ����
+    public string targetTag;
 
     private void Start()
     {
